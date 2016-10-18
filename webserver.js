7224a6f6f518a07a5f1e35d483ec1e202ca7ff98
Joel B. Martins <joel.martins@gmail.com>
2016-10-18 16:45:57 -0500

    added morgan logging

--- webserver.js
+++ webserver.js
@@ -14,6 +14,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3001);
 
 app.use(express.static(__dirname + '/public'));
+app.use(require('morgan')('dev'));
 
 app.get('/', function (req, res) {
     res.render('home');

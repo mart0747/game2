d18c0c8d0889d92fed064cce6a40a5a5e20a5082
Joel B. Martins <joel.martins@gmail.com>
2016-10-18 16:39:37 -0500

    added a headers route

--- webserver.js
+++ webserver.js
@@ -25,6 +25,13 @@ app.get('/about', function (req, res) {
     });
 });
 
+app.get('/headers', function (req, res) {
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    for (var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
+});
+
 //custom 404 page
 app.use(function (req, res, next) {
     res.status(404);

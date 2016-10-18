0a326cacc3e8024375f9a86f9c34fe1c5eba703f
Joel B. Martins <joel.martins@gmail.com>
2016-10-18 15:37:46 -0500

    added handlebars views and templates

--- webserver.js
+++ webserver.js
@@ -2,39 +2,36 @@ var express = require('express');
 var app = express();
 
 //setup handlebars view engine
-var handlebars = require('express-handlebars');
-handlebars.create({defaultLayout: 'main'});
+var handlebars = require('express-handlebars').create({
+    defaultLayout: 'main'
+});;
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3001);
 
-app.get('/', function(req,res){
-    res.type('text/plain');
-    res.send("Let's play some dominion");
+app.get('/', function (req, res) {
+    res.render('home');
 });
 
-app.get('/about', function(req,res){
-    res.type('text/plain');
-    res.send("About Dominion");
+app.get('/about', function (req, res) {
+    res.render('about');
 });
 
 //custom 404 page
 app.use(function (req, res, next) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 //custom 500 page
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' + app.get('port') + ';press Crl-C to terminate.');
 });
-

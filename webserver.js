c1e77d605d9c81db556d099cfb233d1d77f7b564
Joel B. Martins <joel.martins@gmail.com>
2016-10-19 15:43:43 -0500

    added get and post api calls

--- webserver.js
+++ webserver.js
@@ -1,3 +1,4 @@
+//first things first, create the app
 var express = require('express');
 var app = express();
 
@@ -6,15 +7,82 @@ var handlebars = require('express-handlebars').create({
     defaultLayout: 'main'
 });
 
-var fortune = require('./lib/fortune.js');
-
+//setup your app stuff and middlewares 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
 app.set('port', process.env.PORT || 3001);
-
 app.use(express.static(__dirname + '/public'));
 app.use(require('morgan')('dev'));
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded( {'extended':'true'} ));
+
+//initialize the DB and DB Connection
+var dbURI = 'mongodb://localhost/dominion';
+var mongoose = require('mongoose');
+var opts = {
+    server: {
+        socketOptions: {
+            keepalive: 1
+        }
+    }
+};
+
+mongoose.connect(dbURI, opts);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+//create the Player Model.
+var Player = require('./models/player.js');
+
+//make the library available
+var fortune = require('./lib/fortune.js');
+
+
+app.get('/api/players', function (req, res) {
+    Player.find(function (err, Players) {
+        if (err) {
+            console.log(err);
+            res.json(300, err);
+        }
+
+        res.status(200).json(Players);
+    });
+});
+
+app.post('/api/player/', function (req, res) {
+    console.log(req.body);
+
+    var newRecord = new Player({
+        name: req.body.name,
+        ranking: req.body.ranking
+    });
+
+    newRecord.save(function (err, a) {
+        if (err) {
+            console.log(err);
+            res.json(err);
+        } else {
+            res.status(200).json({
+                id: a._id
+            });
+        }
+    });
+
+});
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -28,7 +96,7 @@ app.get('/about', function (req, res) {
 
 app.get('/headers', function (req, res) {
     res.set('Content-Type', 'text/plain');
-    var s = '';
+    var s = 'Request Headers:' + '\n';
     for (var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
     res.send(s);
 });
